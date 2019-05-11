13713edaeee06aa46c57d85c6efe959f35040fe0
Claude Christ <christ.tchambila@gmail.com>
2019-05-11 14:23:06 +0200

    ADD functionalities added to the app

--- src/SalesManagement/Main.java
+++ src/SalesManagement/Main.java
@@ -2,32 +2,37 @@ package SalesManagement;
 
 import javafx.animation.Animation;
 import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.effect.Glow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+
 /**
  * Main.java
- *
+ * <p>
  * This is the main class of the project.
  * it comprises the entry point to the program.
  *
@@ -36,16 +41,39 @@ import javafx.util.Duration;
  */
 public class Main extends Application {
 
+    // ArrayList containing Food items
+    private static ArrayList<FoodMenuItem> foodMenuItems;
+
     // Object of AppMenu that initiates the menu containing the main functionalities
     AppMenu appMenu;
 
     public static void main(String[] args) {
+
+        foodMenuItems = new ArrayList<>();
+
+        // adding suggested food items
+        foodMenuItems.add(new FoodMenuItem("Cheese burger", "main meal", 11.50, new Sale()));
+        foodMenuItems.add(new FoodMenuItem("Gatsby ", "main meal", 8.50, new Sale()));
+        foodMenuItems.add(new FoodMenuItem("Cappuccino", "drinks", 10.25, new Sale()));
+        foodMenuItems.add(new FoodMenuItem("Hot wings", "starter", 6.00, new Sale()));
+        foodMenuItems.add(new FoodMenuItem("Fish and chips", "main meal", 9.50, new Sale()));
+        foodMenuItems.add(new FoodMenuItem("Coffee", "drinks", 5.00, new Sale()));
+        foodMenuItems.add(new FoodMenuItem("Calamari", "starter", 5.50, new Sale()));
+
+
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Sales Management");
+        primaryStage.setOnCloseRequest(event -> {
+            event.consume();
+            boolean exit = ExitDialog.display();
+
+            if (exit)
+                primaryStage.close();
+        });
 
         Pane root = new Pane();
         root.setPrefSize(850, 570);
@@ -69,7 +97,7 @@ public class Main extends Application {
         promptFadeTransition.setCycleCount(Animation.INDEFINITE);
         promptFadeTransition.play();
 
-        Pane.layoutInArea(appMenu, 225, 210, 430, 280, 0, null, true, true, HPos.LEFT, VPos.TOP, true);
+        Pane.layoutInArea(appMenu, 225, 210, 500, 280, 0, null, true, true, HPos.LEFT, VPos.TOP, true);
 
         Pane.layoutInArea(promptText, 290, 300, 200, 50, 0, null, true, true, HPos.LEFT, VPos.TOP, true);
 
@@ -115,7 +143,6 @@ public class Main extends Application {
         private Text text;
 
         /**
-         *
          * @param name name of labelled on the button
          */
         public MenuButton(String name) {
@@ -165,7 +192,7 @@ public class Main extends Application {
 
             setOnMouseEntered(event -> {
                 path.setTranslateX(10);
-                text.setTranslateY(5);
+                text.setTranslateY(4);
                 path.setFill(Color.WHITE);
                 text.setFill(Color.BLACK);
             });
@@ -186,27 +213,98 @@ public class Main extends Application {
 
     }
 
+    private static class ExitDialog {
+
+        static boolean exit;
+
+        public static boolean display() {
+            Stage window = new Stage();
+
+            exit = false;
+
+            window.initModality(Modality.APPLICATION_MODAL);
+            window.setTitle("Exit");
+            window.setMinWidth(250);
+            window.setOnCloseRequest(event -> {
+                event.consume();
+                window.close();
+            });
+            window.setResizable(false);
+
+
+            Label label = new Label("Are you sure you want to exit?");
+
+            Button yesButton = new Button("YES");
+            yesButton.setOnAction(event -> {
+                exit = true;
+                window.close();
+            });
+
+            Button noButton = new Button("NO");
+            noButton.setOnAction(event -> {
+                exit = false;
+                window.close();
+            });
+
+            HBox hBox = new HBox(10, yesButton, noButton);
+            hBox.setAlignment(Pos.CENTER);
+
+            GridPane gridPane = new GridPane();
+
+            gridPane.setPadding(new Insets(10));
+            gridPane.setAlignment(Pos.CENTER);
+            gridPane.setVgap(10);
+
+            GridPane.setConstraints(label, 0, 0);
+            GridPane.setConstraints(hBox, 0, 1);
+
+            gridPane.getChildren().addAll(label, hBox);
+
+            Scene scene = new Scene(gridPane);
+
+            window.setScene(scene);
+            window.showAndWait();
+
+            return exit;
+        }
+
+    }
+
     /**
-     *
      * This class builds the menu for the app
      */
     private class AppMenu extends Parent {
 
         public AppMenu() {
             VBox firstMenu = new VBox(15);
-            VBox secondMenu = new VBox(15);
+            Pane addMenu = new Pane();
 
             firstMenu.setTranslateX(100);
             firstMenu.setTranslateY(200);
 
-            secondMenu.setTranslateX(100);
-            secondMenu.setTranslateY(200);
+            addMenu.setTranslateX(100);
+            addMenu.setTranslateY(200);
 
             final int offset = 400;
-            secondMenu.setTranslateX(offset);
+            addMenu.setTranslateX(offset);
 
+            // ADD button and listener
             MenuButton addButton = new MenuButton("ADD FOOD ITEM");
-            // TODO: ADD addButton listener
+            addButton.setOnMouseClicked(event -> {
+                getChildren().add(addMenu);
+
+                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), firstMenu);
+                tt.setToX(firstMenu.getTranslateX() - offset);
+
+
+                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5), addMenu);
+                tt1.setToX(firstMenu.getTranslateX() - 60);
+
+                tt.play();
+                tt1.play();
+
+                tt.setOnFinished(event1 -> getChildren().remove(firstMenu));
+            });
 
             MenuButton deleteButton = new MenuButton("DELETE FOOD ITEM");
 
@@ -216,9 +314,90 @@ public class Main extends Application {
 
             MenuButton reportButton = new MenuButton("SALES REPORT");
 
+            // Exit button and listener
             MenuButton exitButton = new MenuButton("EXIT");
+            exitButton.setOnMouseClicked(event -> {
+                boolean exit = ExitDialog.display();
+
+                if (exit)
+                    System.exit(0);
+            });
 
             firstMenu.getChildren().addAll(addButton, deleteButton, sortButton, transactionButton, reportButton, exitButton);
+
+            // creating components of addMenu
+            Rectangle rectangle = new Rectangle(480, 250);
+            rectangle.setFill(Color.BLACK);
+            rectangle.setOpacity(0.6);
+
+            // ADD_MENU Layout
+            VBox vBox = new VBox(45);
+            vBox.setPadding(new Insets(15, 0, 0, 0));
+            vBox.setAlignment(Pos.CENTER);
+
+            // ADD_MENU HEADER
+            Text addMenuHeader = new Text("ADD FOOD ITEM");
+            addMenuHeader.setFill(Color.rgb(245, 245, 245));
+            addMenuHeader.setFont(addMenuHeader.getFont().font(18));
+
+            HBox hBox = new HBox(10);
+            hBox.setAlignment(Pos.CENTER);
+
+            TextField itemName = new TextField();
+            itemName.setPromptText("item's name");
+            HBox.setMargin(itemName, new Insets(0, 0, 0, 20));
+
+            ComboBox<String> itemsCategory = new ComboBox<>();
+            itemsCategory.setPromptText("item's category");
+            itemsCategory.getItems().addAll("main meal", "drinks", "starter");
+
+            TextField itemPrice = new TextField();
+            itemPrice.setPromptText("item's price");
+
+            hBox.getChildren().addAll(itemName, itemsCategory, itemPrice);
+
+            HBox hBox1 = new HBox(15);
+            hBox1.setAlignment(Pos.CENTER);
+
+            // ADD item to the menu list
+            Button addFoodItem = new Button("ADD");
+            addFoodItem.setOnAction(event -> {
+                FoodMenuItem item = new FoodMenuItem(itemName.getText(), itemsCategory.getValue(),
+                        Double.parseDouble(itemPrice.getText()), new Sale());
+
+                Main.foodMenuItems.add(item);
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText(null);
+                alert.setContentText("Food item '" + itemName.getText() + "' has been added to the menu list.");
+                alert.showAndWait();
+            });
+
+
+            Button backToMainMenu = new Button("BACK");
+            backToMainMenu.setOnAction(event -> {
+                getChildren().add(firstMenu);
+
+                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25), addMenu);
+                tt.setToX(addMenu.getTranslateX() + offset);
+
+
+                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5), firstMenu);
+                tt1.setToX(addMenu.getTranslateX() + 60);
+
+                tt.play();
+                tt1.play();
+
+                tt.setOnFinished(event1 -> getChildren().remove(addMenu));
+            });
+
+            hBox1.getChildren().addAll(addFoodItem, backToMainMenu);
+
+            vBox.getChildren().addAll(addMenuHeader, hBox, hBox1);
+
+
+            addMenu.getChildren().addAll(rectangle, vBox);
+
 
             getChildren().addAll(firstMenu);
         }
